e6103b4ce537ace8a3243e4dfd15d5f2ee89429f
vincesanityyy <vbustillo97@gmail.com>
2020-12-09 00:26:26 +0800

    updates

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Products;
 class OrderDetails extends Model
 {
     use HasFactory;
     protected $table = 'order_details';
     protected $guarded = [];
+    protected $appends = [
+        'image_link'
+    ];
+
+    public function getImageLinkAttribute()
+    {
+        $image = Products::where('id',$this->product_id)->first();
+        return $image->image_link;
+    }
 }
