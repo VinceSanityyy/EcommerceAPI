1f836e41bf1482366964c0e5fbfc501595430054
vincesanityyy <vbustillo97@gmail.com>
2020-12-13 22:30:38 +0800

    seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,33 +14,42 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $users = [ 
-            [
-              'name' => 'Admin',
-              'email' => 'test@ecommerce.com',
-              'password' => \Hash::make('123123123'),
-              'address' => 'Davao City',
-              'contact' => '0912345678',
-              'role_id' => 1,
-              'isVerified' => 1
-            ],
-            [
-                'name' => 'Customer ',
-                'email' => 'customer@ecommerce.com',
-                'password' => \Hash::make('123123123'),
-                'address' => 'Davao City',
-                'contact' => '09123452678',
-                'role_id' => 2,
-                'isVerified' => 1
-            ],
+            // [
+            //   'name' => 'Admin',
+            //   'email' => 'test@ecommerce.com',
+            //   'password' => \Hash::make('123123123'),
+            //   'address' => 'Davao City',
+            //   'contact' => '0912345678',
+            //   'role_id' => 1,
+            //   'isVerified' => 1
+            // ],
+            // [
+            //     'name' => 'Customer ',
+            //     'email' => 'customer@ecommerce.com',
+            //     'password' => \Hash::make('123123123'),
+            //     'address' => 'Davao City',
+            //     'contact' => '09123452678',
+            //     'role_id' => 2,
+            //     'isVerified' => 1
+            // ],
             [
                 'name' => 'Customer Second',
-                'email' => 'customer2@ecommerce.com',
+                'email' => 'customer3@ecommerce.com',
                 'password' => \Hash::make('123123123'),
                 'address' => 'Davao City',
-                'contact' => '091213452678',
+                'contact' => '09121345267822',
                 'role_id' => 2,
                 'isVerified' => 1
-            ]
+            ],
+            [
+                'name' => 'Customer Third',
+                'email' => 'customer3@ecommerce.com',
+                'password' => \Hash::make('123123123'),
+                'address' => 'Davao City',
+                'contact' => '09121345267822',
+                'role_id' => 2,
+                'isVerified' => 1
+            ],
         ];
         
         \DB::table('users')->insert($users);
