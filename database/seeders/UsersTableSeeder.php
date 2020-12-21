44ea10d1b6c3c232f68f63d63996a2dd3e0dbd3f
AbingPj <pj.abing@gmail.com>
2020-12-21 22:19:43 +0800

    Updated Databse  seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,31 +13,31 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = [ 
-            // [
-            //   'name' => 'Admin',
-            //   'email' => 'test@ecommerce.com',
-            //   'password' => \Hash::make('123123123'),
-            //   'address' => 'Davao City',
-            //   'contact' => '0912345678',
-            //   'role_id' => 1,
-            //   'isVerified' => 1
-            // ],
-            // [
-            //     'name' => 'Customer ',
-            //     'email' => 'customer@ecommerce.com',
-            //     'password' => \Hash::make('123123123'),
-            //     'address' => 'Davao City',
-            //     'contact' => '09123452678',
-            //     'role_id' => 2,
-            //     'isVerified' => 1
-            // ],
+        $users = [
             [
-                'name' => 'Customer Second',
-                'email' => 'customer3@ecommerce.com',
+              'name' => 'Admin',
+              'email' => 'test@ecommerce.com',
+              'password' => \Hash::make('123123123'),
+              'address' => 'Davao City',
+              'contact' => '0912345678',
+              'role_id' => 1,
+              'isVerified' => 1
+            ],
+            [
+                'name' => 'Customer One',
+                'email' => 'customer1@ecommerce.com',
                 'password' => \Hash::make('123123123'),
                 'address' => 'Davao City',
-                'contact' => '09121345267822',
+                'contact' => '09123452678',
+                'role_id' => 2,
+                'isVerified' => 1
+            ],
+            [
+                'name' => 'Customer Second',
+                'email' => 'customer2@ecommerce.com',
+                'password' => \Hash::make('123123123'),
+                'address' => 'Davao City',
+                'contact' => '09121231234',
                 'role_id' => 2,
                 'isVerified' => 1
             ],
@@ -46,12 +46,12 @@ class UsersTableSeeder extends Seeder
                 'email' => 'customer3@ecommerce.com',
                 'password' => \Hash::make('123123123'),
                 'address' => 'Davao City',
-                'contact' => '09121345267822',
+                'contact' => '09121231235',
                 'role_id' => 2,
                 'isVerified' => 1
             ],
         ];
-        
+
         \DB::table('users')->insert($users);
     }
 }
